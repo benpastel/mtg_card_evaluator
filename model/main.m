8351749776d600ee83844bd96f1aebdb426d93d2
Ben Pastel <benpastel@gmail.com>
2015-11-06 12:21:37 -0800

    Give up on making main a unix function.

--- model/main.m
+++ model/main.m
@@ -1,14 +1,14 @@
-function [] = main( X_path, Y_path )
-%MAIN
 % So far, this just trains a linear model on the input files
 % and prints some stats about it
 %
 % The input files can be in any format 'load' can read
 % e.g. space-separated floats with one training example per line.
 
-    X = load(X_path);
-    Y = load(Y_path);
+X_path = 'feature_matrix.txt';
+X = load(X_path);
 
-    [theta, rmse] = linear_regression(X, Y)
-end
+% Y is random until we have price data
+Y = rand(size(X, 1), 1);
+
+[theta, rmse] = linear_regression(X, Y)
 
